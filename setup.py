4fdc11dd7995804d5ce2ce3395cf42d1059fd9a7
Andrei Lapets <a@lapets.io>
2020-08-11 00:26:16 -0400

    Redefine versioning scheme; update README/package files.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name             = 'lagrange',
-    version          = '0.1.0.0',
-    packages         = ['lagrange',],
-    install_requires = [],
-    license          = 'MIT',
-    url              = 'https://github.com/lapets/lagrange',
-    author           = 'Andrei Lapets',
-    author_email     = 'a@lapets.io',
-    description      = 'Python library with a basic native implementation of Lagrange interpolation over finite fields.',
-    long_description = open('README.rst').read(),
-    test_suite       = 'nose.collector',
-    tests_require    = ['nose'],
+    name="lagrange",
+    version="0.2.0",
+    packages=["lagrange",],
+    install_requires=[],
+    license="MIT",
+    url="https://github.com/lapets/lagrange",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Python library with a basic native implementation "+\
+                "of Lagrange interpolation over finite fields.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
 )
