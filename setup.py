1a2ab607357c2cfbc27d993094419217a2b57865
Andrei Lapets <a@lapets.io>
2022-06-13 15:31:01 -0400

    Use pytest for tests and setuptools for dependencies; update README/docs/CI.

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 from setuptools import setup
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+with open('README.rst', 'r') as fh:
+    long_description = fh.read().replace('.. include:: toc.rst\n\n', '')
 
 # The lines below can be parsed by `docs/conf.py`.
-name = "lagrange"
-version = "0.3.0"
+name = 'lagrange'
+version = '0.4.0'
 
 setup(
     name=name,
     version=version,
     packages=[name,],
     install_requires=[],
-    license="MIT",
-    url="https://github.com/lapets/lagrange",
-    author="Andrei Lapets",
-    author_email="a@lapets.io",
-    description="Python library with a basic native implementation "+\
-                "of Lagrange interpolation over finite fields.",
+    extras_require={
+        'docs': [
+            'sphinx~=4.2.0',
+            'sphinx-rtd-theme~=1.0.0'
+        ],
+        'test': [
+            'pytest~=7.0',
+            'pytest-cov~=3.0'
+        ],
+        'lint': [
+            'pylint~=2.14.0'
+        ],
+        'coveralls': [
+            'coveralls~=3.3.1'
+        ],
+        'publish': [
+            'setuptools~=62.0',
+            'wheel~=0.37',
+            'twine~=4.0'
+        ]
+    },
+    license='MIT',
+    url='https://github.com/lapets/lagrange',
+    author='Andrei Lapets',
+    author_email='a@lapets.io',
+    description='Python library with a basic native implementation ' + \
+                'of Lagrange interpolation over finite fields.',
     long_description=long_description,
-    long_description_content_type="text/x-rst",
-    test_suite="nose.collector",
-    tests_require=["nose"],
+    long_description_content_type='text/x-rst',
 )
