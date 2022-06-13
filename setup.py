16cccea8faa15829c044b7acf353313135f3e365
Andrei Lapets <a@lapets.io>
2022-06-13 18:34:39 -0400

    Adjust code formatting delimiter in comment in package file.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open('README.rst', 'r') as fh:
     long_description = fh.read().replace('.. include:: toc.rst\n\n', '')
 
-# The lines below can be parsed by `docs/conf.py`.
+# The lines below can be parsed by ``docs/conf.py``.
 name = 'lagrange'
 version = '1.0.0'
 
