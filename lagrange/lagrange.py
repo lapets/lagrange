e4c157e2a69c3a552ddaecf88c86d51745ebf8a4
Andrei Lapets <a@lapets.io>
2022-06-13 18:02:03 -0400

    Support broad range of collection types; update docstring and package version.

--- lagrange/lagrange.py
+++ lagrange/lagrange.py
@@ -2,6 +2,9 @@
 Python library with a basic native implementation of Lagrange
 interpolation over finite fields.
 """
+from __future__ import annotations
+from typing import Union
+from collections.abc import Iterable, Sequence
 import doctest
 
 def _inv(a, prime):
@@ -10,10 +13,15 @@ def _inv(a, prime):
     """
     return pow(a, prime - 2, prime)
 
-def interpolate(points, prime):
+def interpolate(
+        points: Union[dict, Sequence[int], Iterable[Sequence[int]]], prime: int
+    ) -> int:
+    # pylint: disable=R0912 # Accommodate large number of branches for type checking.
     """
-    Determine the value at ``x = 0`` given a list of pairs or a
-    dictionary mapping indices to values.
+    Determine the value at the origin of the domain (*e.g.*, where *x* = 0)
+    given a collection of points. The point information can be represented as
+    a collection of two-component coordinates, as a dictionary, or as a sequence
+    of values.
 
     >>> interpolate([(1, 15), (2, 9), (3, 3)], 17)
     4
@@ -43,13 +51,54 @@ def interpolate(points, prime):
     ... )
     123
 
-    Exceptions are raised if the supplied arguments are not of the
-    expected types.
+    .. |Iterable| replace:: ``Iterable``
+    .. _Iterable: https://docs.python.org/3/library/collections.abc.html#collections.abc.Iterable
 
+    .. |Sequence| replace:: ``Sequence``
+    .. _Sequence: https://docs.python.org/3/library/collections.abc.html#collections.abc.Sequence
+
+    If the point information is supplied as an |Iterable| of integers, that iterable
+    must be a |Sequence|_.
+
+    >>> interpolate({15, 9, 3}, 17)
+    Traceback (most recent call last):
+      ...
+    TypeError: iterable of integers that represents points must be a sequence
+
+    Alternatively, a two-coordinate |Sequence|_ can be used to represent each individual point.
+    In that case, any |Iterable|_ of such individual points is supported.
+
+    >>> interpolate({(1, 15), (2, 9), (3, 3)}, 17)
+    4
+
+    At least one point must be supplied.
+
+    >>> interpolate([], 17)
+    Traceback (most recent call last):
+      ...
+    ValueError: at least one point is required
+
+    An exception is raised if a supplied argument (or a component thereof) does not
+    have the expected structure or is not of the expected type.
+
+    >>> interpolate({1: 15.0, 'a': 9, 'b': 3}, 17)
+    Traceback (most recent call last):
+      ...
+    TypeError: dictionary that represents points must have integer keys and values
+    >>> interpolate({(1, 15, 0), (2, 9, 0), (3, 3, 0)}, 17)
+    Traceback (most recent call last):
+      ...
+    TypeError: iterable that represents points must contain integers or two-element \
+sequences of integers
     >>> interpolate('abc', 123)
     Traceback (most recent call last):
       ...
-    TypeError: expecting a list of values, list of points, or a dictionary
+    TypeError: iterable that represents points must contain integers or two-element \
+sequences of integers
+    >>> interpolate(1.23, 123)
+    Traceback (most recent call last):
+      ...
+    TypeError: expecting dictionary or iterable that represents points
     >>> interpolate([15, 9, 3], 'abc')
     Traceback (most recent call last):
       ...
@@ -59,16 +108,49 @@ def interpolate(points, prime):
       ...
     ValueError: expecting a positive integer prime modulus
     """
-    if isinstance(points, list) and all(isinstance(p, int) for p in points):
-        points = dict(zip(range(1, len(points) + 1), points))
-    elif isinstance(points, (list, set, tuple)) and\
-       len(points) > 0 and\
-       all(isinstance(p, (list, tuple)) and len(p) == 2 for p in points):
-        points = dict([tuple(p) for p in points])
-    elif isinstance(points, dict):
-        pass
-    else:
-        raise TypeError('expecting a list of values, list of points, or a dictionary')
+    values = None # Initially, assume that the supplied point data is not valid.
+
+    if isinstance(points, dict):
+        if not (
+            all(isinstance(k, int) for k in points.keys()) and \
+            all(isinstance(v, int) for v in points.values())
+        ):
+            raise TypeError(
+                'dictionary that represents points must have integer keys and values'
+            )
+
+        values = points # Valid representation.
+
+    elif isinstance(points, Iterable):
+        is_sequence = isinstance(points, Sequence) # In case iterable contains only integers.
+        entries = list(points)
+
+        if all(isinstance(e, int) for e in entries):
+            if not is_sequence:
+                raise TypeError(
+                    'iterable of integers that represents points must be a sequence'
+                )
+
+            values = dict(zip(range(1, len(entries) + 1), entries)) # Valid representation.
+
+        elif all(isinstance(e, Sequence) for e in entries):
+            entries = [tuple(e) for e in entries]
+            if not all(
+                len(e) == 2 and all(isinstance(c, int) for c in e)
+                for e in entries
+            ):
+                raise TypeError(
+                    'iterable that represents points must contain integers ' + \
+                    'or two-element sequences of integers'
+                )
+
+            values = dict(entries) # Valid representation.
+
+    if values is None:
+        raise TypeError('expecting dictionary or iterable that represents points')
+
+    if len(values) == 0:
+        raise ValueError('at least one point is required')
 
     if not isinstance(prime, int):
         raise TypeError('expecting an integer prime modulus')
@@ -78,15 +160,15 @@ def interpolate(points, prime):
 
     # Compute the Lagrange coefficients at ``0``.
     coefficients = {}
-    for i in range(1, len(points) + 1):
+    for i in range(1, len(values) + 1):
         coefficients[i] = 1
-        for j in range(1, len(points) + 1):
+        for j in range(1, len(values) + 1):
             if j != i:
                 coefficients[i] = (coefficients[i] * (0 - j) * _inv(i - j, prime)) % prime
 
     value = 0
-    for i in range(1, len(points)+1):
-        value = (value + points[i] * coefficients[i]) % prime
+    for i in range(1, len(values) + 1):
+        value = (value + values[i] * coefficients[i]) % prime
 
     return value
 
